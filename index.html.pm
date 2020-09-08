f0f96f0e5c1ddd5846af813307beb67d73032856
Stig Johan Berggren <stigjb@gmail.com>
2020-09-08 21:36:27 +0200

    Flytt til rotmappa

--- index.html.pm
+++ index.html.pm
@@ -3,6 +3,12 @@
 Denne nettsiden er laget med
 ◊link["https://docs.racket-lang.org/pollen/"]{Pollen}.
 
+◊h2{Artikler}
+
+◊ul{
+  ◊li{◊link["konvekse-mangekanter.html"]{Konvekse mangekanter}}
+}
+
 ◊h2{Lenker til ting}
 
 ◊ul{
@@ -14,4 +20,5 @@ Denne nettsiden er laget med
 
 ◊ul{
   ◊li{◊link["https://lib.rs/crate/tonality"]{Tonality}: Musikkteori i Rust}
+  ◊li{◊link["/chord-quiz"]{Chord quiz}: Øv på å gjenkjenne akkorder i nettleseren}
 }
