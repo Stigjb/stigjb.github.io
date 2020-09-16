f89c96dde8825ea60a8f03bf9bec502c61c98f7c
Stig Johan Berggren <stigjb@gmail.com>
2020-09-16 15:54:38 +0200

    ny side med anbefalinger

---
+++ anbefalinger.html.pm
@@ -0,0 +1,13 @@
+#lang pollen
+
+Anbefalt lesestoff på Internett
+
+En samling digitale bøker som har imponert meg. Større enn blogger.
+
+◊["https://meaningness.com"]{Meaningness}
+
+◊["https://practicaltypography.com"]{Practical Typography}
+
+◊["https://quantum.country"]{Quantum Country}
+
+◊["https://meaningness.com"]{Meaningness}
