8a65a76f0dfa51bbaa625a1f0262ae04a48e157c
Stig Johan Berggren <stig@wrepit.no>
2024-01-29 10:22:59 +0100

    feat: random tagline

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,16 +1,22 @@
 <script>
 	import { page } from '$app/stores';
 	const title = $page.data.title;
+	const taglines = [
+		'et konsept fra Stig',
+		'vi gir oss aldri på Stig',
+		'ikke alle kan kalle seg det samme som Stig'
+	];
+	const subtitle = taglines[Math.floor(Math.random() * taglines.length)];
 </script>
 
 <header>
 	<div>
 		<h1>{title ? `${title} | ` : ''}Stig Johan</h1>
-		<p>Stig Johan Berggren</p>
+		<p>{subtitle}</p>
 	</div>
 </header>
 
-<style>
+<style lang="scss">
 	header {
 		width: 100%;
 		padding: 1rem 0.5rem;
@@ -18,14 +24,21 @@
 		background: var(--color-secondary-1-3);
 		color: var(--color-secondary-1-1);
 		border-bottom: solid 5px var(--color-secondary-1-2);
-	}
-	header div {
-		max-width: var(--content-width);
-		margin: auto;
-		padding: 0.5rem;
-		border-radius: 0.25rem;
-	}
-	header div p:last-child {
-		margin-bottom: 0;
+		div {
+			max-width: var(--content-width);
+			margin: auto;
+			display: flex;
+			flex-flow: column nowrap;
+			align-items: center;
+			padding: 0.5rem;
+			border-radius: 0.25rem;
+			h1 {
+				font-size: 3rem;
+			}
+			p:last-child {
+				font-style: italic;
+				margin-bottom: 0;
+			}
+		}
 	}
 </style>
