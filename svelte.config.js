1487c1c304002a62b5473ae6361269a30b761be1
Stig Johan Berggren <stig@wrepit.no>
2024-02-03 15:59:34 +0100

    fix base path

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : 'process.env.BASE_PATH'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	},
 	extensions: ['.svelte', '.md'],
